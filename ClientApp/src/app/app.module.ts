5912557c38b1621ce00b1161d7733ec735374baa
Aries Camitan <arc@sitecore.net>
2020-01-21 10:23:38 +0800

    Add angular material module

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,31 +3,34 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { GeolocationService } from './geolocation.service'
 import { DataService } from './data.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
+         MatToolbarModule, MatCardModule, MatSlideToggleModule } from "@angular/material";
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,  
+    MatButtonModule, 
+    MatIconModule, 
+    MatInputModule, 
+    MatSelectModule, 
+    MatSliderModule,
+    MatToolbarModule, 
+    MatCardModule, 
+    MatSlideToggleModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      //{ path: '', component: HomeComponent, pathMatch: 'full' },
     ])
   ],
   providers: [GeolocationService, DataService],
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # AngularPWADotNetCore
-Sample PWA App using Angular with ASP.Net Core 3.1
+Sample PWA App using Angular 8 with ASP.Net Core 3.1
+
+## Angular Material 
+
+### Theming
+https://material.angular.io/guide/theming
 
 
